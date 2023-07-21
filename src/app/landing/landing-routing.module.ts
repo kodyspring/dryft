b3eae250a510305857ee5b511d65350b70208858
kodyspring <kody.spring@gmail.com>
2023-07-21 00:46:44 -0400

    add landing page

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
+import { LandingComponent } from './landing.component';
 
 const routes: Routes = [
-  { path: "", component: LandingComponent},
   { path: "**", component: LandingComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class LandingRoutingModule { }
