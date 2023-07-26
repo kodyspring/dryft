52d0041c42d7ac92da63971bbc369bd44d7171cf
Kody Spring <36212983+kodyspring@users.noreply.github.com>
2023-07-26 00:09:09 -0400

    update nav routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
   { path: "", loadChildren: () => import('./pages/landing/landing.module').then((m) => m.LandingModule), data: { breadcrumb: 'home' }, },
