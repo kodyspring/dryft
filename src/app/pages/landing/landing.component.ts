1d2a88f2b8bc3cfb598bfb409648c29f8b2d9643
Kody Spring <kody.spring@gmail.com>
2024-02-26 22:29:37 -0500

    changes for jspdf added to make a pdf of dom element

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import {jsPDF} from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-landing',
@@ -7,6 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing.component.less']
 })
 export class LandingComponent {
+
+  @ViewChild('PDF', {static:false}) el!: ElementRef;
 
   aboutPath: string = "about";
   contactPath: string = "contact";
@@ -23,7 +27,28 @@ export class LandingComponent {
   }
 
   navigate(path: string): void {
-    this.router.navigateByUrl(`/${path}`)
+    this.router.navigateByUrl(`/${path}`);
+  }
+
+  makePdf() {
+    let pdf = new jsPDF('p', 'cm', 'a4');
+
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("sample.pdf");
+      }
+    })
+  }
+
+  public convertToPdf() {
+    html2canvas(this.el.nativeElement).then(canvas => {
+      const contentDataUrl = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'a4');
+      var width = pdf.internal.pageSize.getWidth();
+      var height = canvas.height * width / canvas.width;
+      pdf.addImage(contentDataUrl, 'PNG', 0, 0, width, height);
+      pdf.save('output.pdf');
+    })
   }
 
 }
